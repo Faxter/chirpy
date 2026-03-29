848aa7f61b3127d01cf4bff989fc739854ad7171
faxter <info@faxxter.com>
2026-03-29 17:44:26 +0200

    add readiness endpoint /healthz

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import "net/http"
 
 func main() {
 	s := http.NewServeMux()
-	s.Handle("/", http.FileServer(http.Dir(".")))
+	s.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	s.HandleFunc("/healthz", readinessEndpoint)
 	serv := new(http.Server)
 	serv.Handler = s
 	serv.Addr = ":8080"
 	serv.ListenAndServe()
+}
+
+func readinessEndpoint(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	responseWriter.WriteHeader(http.StatusOK)
+	responseWriter.Write([]byte("OK"))
 }
