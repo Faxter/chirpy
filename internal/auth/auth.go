c3d34d4299f667acfed6a7c902d03af340f14e56
faxter <info@faxxter.com>
2026-05-11 13:51:31 +0200

    add making and validating JWT to auth package

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"fmt"
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -16,4 +23,40 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, err
 	}
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	signedString, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return signedString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	type MyCustomClaims struct {
+		jwt.RegisteredClaims
+	}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&MyCustomClaims{},
+		func(token *jwt.Token) (any, error) { return []byte(tokenSecret), nil })
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	if claims, ok := token.Claims.(*MyCustomClaims); ok {
+		id, err := uuid.Parse(claims.Subject)
+		if err != nil {
+			return uuid.UUID{}, err
+		}
+		return id, nil
+	} else {
+		return uuid.UUID{}, fmt.Errorf("cannot process claims")
+	}
 }
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func TestCreateJWT(t *testing.T) {
+	_, err := MakeJWT(uuid.New(), "supersecret", 1*time.Second)
+	if err != nil {
+		t.Errorf("could not create JWT! %s", err)
+	}
+}
+
+func TestValidateJWT(t *testing.T) {
+	userId := uuid.New()
+	jwt, _ := MakeJWT(userId, "supersecret", 1*time.Second)
+	validatedId, err := ValidateJWT(jwt, "supersecret")
+	if err != nil {
+		t.Errorf("validation of JWT failed: %s", err)
+	}
+	if userId != validatedId {
+		t.Errorf("id mismatch: %s vs. %s", userId, validatedId)
+	}
+}
+
+func TestValidateExpiredJWT(t *testing.T) {
+	userId := uuid.New()
+	jwt, _ := MakeJWT(userId, "supersecret", 1*time.Microsecond)
+
+	time.Sleep(10 * time.Microsecond)
+
+	_, err := ValidateJWT(jwt, "supersecret")
+	if err == nil {
+		t.Errorf("expected token to be expired!")
+	}
+}
+
+func TestValidateSignedJWTWithDifferentKey(t *testing.T) {
+	userId := uuid.New()
+	jwt, _ := MakeJWT(userId, "supersecret", 1*time.Second)
+	_, err := ValidateJWT(jwt, "differentKey")
+	if err == nil {
+		t.Errorf("expected validation to fail due to different sign key!")
+	}
+}
